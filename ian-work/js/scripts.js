49851bab99943c8cf3999f57e01cc84add080ed1
Samuel <sammythige26@gmail.com>
2019-05-16 18:33:26 +0300

    navigation bar on all pages

---
+++ ian-work/js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $("#parrot").click(function() {
+        $("#paragraph").toggle();
+    });
+    $("#mirror").click(function() {
+        $("#paragraph1").toggle();
+    });
+    $("#mirror1").click(function() {
+        $("#paragraph2").toggle();
+    });
+    $("#mirror2").click(function() {
+        $("#paragraph3").toggle();
+    });
+    $("#mirror3").click(function() {
+        $("#paragraph4").toggle();
+    });
+    $("#mirror4").click(function() {
+        $("#paragraph5").toggle();
+    });
+    $("#mirror5").click(function() {
+        $("#paragraph6").toggle();
+    });
+    $("#mirror6").click(function() {
+        $("#paragraph7").toggle();
+    });
+
+});
